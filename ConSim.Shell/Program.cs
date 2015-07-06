650600d5f99ba3197b1a7bfb1eedf78e645f8468
Howard Beard-Marlowe <howardbm@live.se>
2015-07-06 08:52:48 +0300

    Begin shell work

--- ConSim.Shell/Program.cs
+++ ConSim.Shell/Program.cs
@@ -1,4 +1,29 @@
-﻿using System;
+﻿//
+//  Program.cs
+//
+//  Author:
+//       Howard Beard-Marlowe <howardbm@live.se>
+//
+//  Copyright (c) 2015 Howard Beard-Marlowe
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+/* INCLUDES */
+#region Includes
+using System;
+using System.Reflection;
+#endregion
 
 namespace ConSim.Shell
 {
@@ -6,7 +31,17 @@ namespace ConSim.Shell
   {
     public static void Main (string[] args)
     {
-      Console.WriteLine ("Hello World!");
+       
+    }
+
+    private string help()
+    {
+      return
+        @"ConSim " + Assembly.GetExecutingAssembly().GetName().Version.ToString()
+      + @"------------------------------------------------------------"
+      + @" -h Displays this help screen" 
+      + @" -l [lessondir] Opens a lesson"
+      + @" - ";
     }
   }
 }
